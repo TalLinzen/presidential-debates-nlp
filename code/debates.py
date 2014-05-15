be689941bc9b38018e4a25ecfcfe145bcadceb30
Tal Linzen <tal.linzen@gmail.com>
2014-05-14 20:19:32 -0400

    Add code to dump docs and run CoreNLP

--- code/debates.py
+++ code/debates.py
@@ -1,19 +1,26 @@
 import csv
 import os
 import re
+import subprocess
 
 header_re = re.compile(r'(?P<doc_id>\d+) of (?P<total_docs>\d+) DOCUMENTS\n+')
 fields = ['byline', 'section', 'length', 'dateline', 'load-date',
           'language', 'graphic', 'publication-type', 'acc-no',
           'journal-code', 'document-type']
-additional_fields = ['year', 'debate_number', 'publication', 'date', 'title',
-                     'text']
+additional_fields = ['year', 'debate_number', 'doc_id', 'publication',
+                     'date', 'title', 'text', 'total_docs']
 
 
 class ArticleParser(object):
 
-    def __init__(self):
+    def __init__(self, docs_dir, annotations_dir, doc_text_dir,
+                 corenlp_dir, corenlp_version):
         self.docs = []
+        self.docs_dir = docs_dir
+        self.annotations_dir = annotations_dir
+        self.doc_text_dir = doc_text_dir
+        self.corenlp_dir = corenlp_dir
+        self.corenlp_version = corenlp_version
 
     def process_body(self, body):
         doc = {}
@@ -53,6 +60,44 @@ class ArticleParser(object):
             self.post_process_doc(doc)
             self.docs.append(doc)
 
+    def dump_to_dir(self):
+        '''
+        Dump all files one by one 
+        '''
+        if not os.path.exists(self.doc_text_dir):
+            os.mkdir(self.doc_text_dir)
+
+        filenames = []
+        for doc in self.docs:
+            filename = '%s_%s_%03d.txt' % (doc['year'], doc['debate_number'],
+                                           int(doc['doc_id']))
+            full_filename = os.path.join(self.doc_text_dir, filename)
+            filenames.append(full_filename)
+            with open(full_filename, 'w') as handle:
+                handle.write(doc['text'])
+
+        file_list_name = os.path.join(self.doc_text_dir, 'file_list.txt')
+        with open(file_list_name, 'w') as handle:
+            handle.write('\n'.join(filenames))
+
+    def run_corenlp(self):
+        '''
+        dirname: path to temporary files created by dump_to_dir.
+        corenlpdir: path to Stanford CoreNLP.
+        '''
+        if not os.path.exists(self.annotations_dir):
+            os.mkdir(self.annotations_dir)
+
+        file_list = os.path.join(self.doc_text_dir, 'file_list.txt')
+        cwd = os.getcwd()
+        os.chdir(self.corenlp_dir)
+
+        try:
+            command = 'java -cp stanford-corenlp-%s.jar:stanford-corenlp-%s-models.jar:xom.jar:joda-time.jar:jollyday.jar:ejml-0.23.jari -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref -filelist %s -outputDirectory %s' % (self.corenlp_version, self.corenlp_version, file_list, self.annotations_dir)
+            subprocess.call(command.split())
+        finally:
+            os.chdir(cwd)
+
     def alldoc_csv(self, filename='/tmp/tmp.csv'):
         '''
         Create a CSV file with all doc metadata and 100 first characters of 
@@ -64,15 +109,11 @@ class ArticleParser(object):
         for doc in self.docs:
             writer.writerow([doc.get(field, '')[:100] for field in all_fields])
 
-
-dirname = os.path.expanduser('~/Dropbox/Debate Coding/data/txt')
-def process_all_presidential_debates(dirname):
-    ap = ArticleParser()
-    for filename in os.listdir(dirname):
-        if filename[0] == '.':
-            continue
-        base, ext = os.path.splitext(filename)
-        year, _, _, debate_number = base.split()
-        ap.process_file(os.path.join(dirname, filename), 
-                        {'year': year, 'debate_number': debate_number})
-    return ap
+    def process_all(self):
+        for filename in os.listdir(self.docs_dir):
+            if filename[0] == '.':
+                continue
+            base, ext = os.path.splitext(filename)
+            year, _, _, debate_number = base.split()
+            self.process_file(os.path.join(self.docs_dir, filename), 
+                              {'year': year, 'debate_number': debate_number})
