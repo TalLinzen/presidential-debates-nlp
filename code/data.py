0a4f1b65cbf44d0fb38e059f2a8e2b56a8228576
Tal Linzen <tal.linzen@gmail.com>
2014-08-10 19:17:03 -0400

    Other -> indep

--- code/data.py
+++ code/data.py
@@ -1,9 +1,8 @@
-parties = ['dem', 'rep', 'other']
+parties = ['dem', 'rep', 'indep']
 
-candidates = {1992: {'dem': 'Clinton', 'rep': 'Bush', 'other': 'Perot'},
+candidates = {1992: {'dem': 'Clinton', 'rep': 'Bush', 'indep': 'Perot'},
               1996: {'dem': 'Clinton', 'rep': 'Dole'},
               2000: {'dem': 'Gore', 'rep': 'Bush'},
               2004: {'dem': 'Kerry', 'rep': 'Bush'},
               2008: {'dem': 'Obama', 'rep': 'McCain'},
               2012: {'dem': 'Obama', 'rep': 'Romney'}}
-
